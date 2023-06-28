101abd617c9b1b272af6eeb31975e527484a6159
Rhama Krisner <rhamakrisner@gmail.com>
2023-06-28 10:27:05 -0300

    Rota de teste

--- src/main/java/application/app/Startup.java
+++ src/main/java/application/app/Startup.java
@@ -1,4 +1,4 @@
-package application.restwithspringbootandjava;
+package application.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/application/app/StartupTests.java
+++ src/test/java/application/app/StartupTests.java
@@ -1,4 +1,4 @@
-package application.restwithspringbootandjava;
+package application.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
